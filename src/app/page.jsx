458639f256581add40551c543b9150152db5d78a
hesamnn <hesamnn@gmail.com>
2024-03-09 10:54:30 +0330

    nc in uni

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
+import {motion} from "framer-motion"
 const Homepage = () => {
   return(
+    <motion.div
+    className="h-full"
+    initial={{y:"-200vh"}}
+    animate={{y:"0%"}}
+    transition={{duration: 1}}
+    >
+
 <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
   {/* IMAGE SECTION */}
   <div className="h-1/2 relative lg:w-1/2 lg:h-full">
@@ -20,7 +28,9 @@ const Homepage = () => {
       <button className="p-4 rounded-lg ring-1 ring-black ">Contact Me</button>
     </div>
   </div>
-</div> 
+</div>
+</motion.div>
+
   );
 };
 
