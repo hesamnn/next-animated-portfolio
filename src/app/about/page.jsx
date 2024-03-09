438dbb19b583e77a1fa7a617955606055b6e7d6d
hesamnn <hesamnn@gmail.com>
2024-03-09 15:06:04 +0330

    last nc in uni

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,32 @@
+"use client"
+import {motion} from "framer-motion"
+
+
 const AboutPage = () =>{
     return(
-        <div className="">About Page</div>
+        <motion.div
+        className="h-full"
+        initial={{y:"-200vh"}}
+        animate={{y:"0%"}}
+        transition={{duration: 1}}
+        >
+            {/* CONAINER */}
+        <div className="">
+        {/* TEXT CONTAINER */}
+            <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 ">
+                {/* BIO GRAPHY */}
+            <div className="">BIO GRAPHY</div>
+            {/* SKILL */}
+            <div className="">SKILL</div>
+            {/* EXPERINCE */}
+            <div className="">EXPERINCE</div>
+
+            </div>
+            {/* SVG CONTAINER */}
+            <div className=""></div>
+        </div>
+        </motion.div>
+
     )
 }
 
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,17 @@
+"use client"
+import {motion} from "framer-motion"
+
 const ContentPage = () =>{
     return(
+        <motion.div
+        className="h-full"
+        initial={{y:"-200vh"}}
+        animate={{y:"0%"}}
+        transition={{duration: 1}}
+        >
         <div className="">Contant Page</div>
+            
+        </motion.div>
     )
 }
 
